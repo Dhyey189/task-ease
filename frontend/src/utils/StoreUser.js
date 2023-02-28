6b6496651a91a93f6a2d3993b737a31d4181ccf2
Dhyey189 <dhyeypatel189@gmail.com>
2023-02-28 15:26:18 +0530

    setting up dashboard page,  using syncfusion

--- frontend/src/utils/StoreUser.js
+++ frontend/src/utils/StoreUser.js
@@ -1,7 +1,7 @@
 import setWithExpiry from "./SetWithExpiry"
 
 const storeUser = (user) => {
-    setWithExpiry("user",user,300000)
+    setWithExpiry("user",user,300000*12)
 }
 
 export default storeUser
