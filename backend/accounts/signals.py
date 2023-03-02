f8de8fd66a885ab83efc292d442da13a752a4785
Dhyey189 <dhyeypatel189@gmail.com>
2023-03-02 09:20:36 +0530

    added signals for post save and pre save operations on models

--- backend/accounts/signals.py
+++ backend/accounts/signals.py
@@ -1,12 +1,18 @@
-from django.db.models.signals import pre_save,post_save
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from accounts.models import User
+from accounts.tasks import task_create_default_categories
 
-@receiver(pre_save, sender = User)
-def create_username(sender,instance,*args,**kwargs):
-    instance.username = str(instance.email)
+# @receiver(pre_save, sender = User)
+# def create_username(sender,instance,*args,**kwargs):
+#     instance.username = str(instance.email)
+#     instance
 
 @receiver(post_save, sender = User)
 def create_default_categories(sender,instance,created,*args,**kwargs):
     if created:
-        # create default categories for the user.
-        pass
+        print("In signal", instance)
+        instance.username = str(instance.email)
+        # create default categories for the user asyncronously using celery.
+        instance.save()
+        task_create_default_categories.delay(instance.id)
